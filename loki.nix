3ac6e134276e5e605c50836159b9ad652151a8f7
Anton Romanova <contact@antonromanova.com>
2024-05-09 09:42:35 +0200

    Remove irrelevant comments from loki.nix

--- loki.nix
+++ loki.nix
@@ -1,8 +1,3 @@
-# TODO: check the validity of this template
-# from https://xeiaso.net/blog/prometheus-grafana-loki-nixos-2020-11-20
-
-# see more this: https://gist.github.com/rickhull/895b0cb38fdd537c1078a858cf15d63e
-
 { config, pkgs,... }: {
   services.loki = {
     enable = true;
